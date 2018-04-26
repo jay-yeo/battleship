9cf693572a3e939f1c9e3455fbc9e732df3ea951
Jason Yeo <jayyeo21@gmail.com>
2018-04-26 15:38:11 +0200

    Implemented user input to set player names

--- src/main/java/com/matritellabs/utama/jbd/Main.java
+++ src/main/java/com/matritellabs/utama/jbd/Main.java
@@ -1,29 +1,43 @@
 package com.matritellabs.utama.jbd;
+import com.matritellabs.utama.helper.LineByLineReader;
 
 public class Main {
     public static void main(String[] args) {
 
         // New Game
+        System.out.println("Starting new Battleship game...");
         Game newGame = new Game();
 
-        // Set Players
-        newGame.setGamePlayer(1,"Daniel");
-        newGame.setGamePlayer(2,"Balint");
+        // Setup Players
 
+        // Player 1
+        System.out.println("Player 1: (Enter Name)");
+        LineByLineReader playerOneNameInput = new LineByLineReader();
+        String playerOneName = playerOneNameInput.readLineFromStdIn();
+        newGame.setGamePlayer(1, playerOneName);
+
+        // Player 2
+        System.out.println("Player 2: (Enter Name)");
+        LineByLineReader playerTwoNameInput = new LineByLineReader();
+        String playerTwoName = playerTwoNameInput.readLineFromStdIn();
+
+        newGame.setGamePlayer(2, playerTwoName);
 
 
         System.out.println(newGame);
 
-        System.out.println("Game View:");
-        Ship battleship = newGame.getPlayerOne().getShip("Battleship");
-        Ship carrier = newGame.getPlayerOne().getShip("Carrier");
-
-
-        newGame.getPlayerOne().placeShip(battleship,0,0, "H");
-        newGame.getPlayerOne().placeShip(carrier,5,0, "V");
-        newGame.getPlayerOne().getPlayerTable().printTable();
-
-        System.out.println(newGame.getPlayerOne().getShip("Battleship"));
+//        System.out.println("Game View:");
+//        Ship battleship = newGame.getPlayerOne().getShip("Battleship");
+//        Ship carrier = newGame.getPlayerOne().getShip("Carrier");
+//
+//
+//        newGame.getPlayerOne().placeShip(battleship,0,0, "H");
+//
+//
+//        newGame.getPlayerOne().placeShip(carrier,5,0, "V");
+//        newGame.getPlayerOne().getPlayerTable().printTable();
+//
+//        System.out.println(newGame.getPlayerOne().getShip("Battleship"));
 
 
 
