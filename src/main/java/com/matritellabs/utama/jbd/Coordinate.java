5411e1344dc283d1bc0fad12af1a236d5ee1ab9e
klibalint <klibalint@mail.eu>
2018-04-26 14:29:38 +0200

    Coordinate class created constructor added

---
+++ src/main/java/com/matritellabs/utama/jbd/Coordinate.java
@@ -0,0 +1,16 @@
+package com.matritellabs.utama.jbd;
+
+public class Coordinate {
+
+    private static int x;
+    private static int y;
+
+    //Constructor
+    public Coordinate (int xCoordinate, int yCoordinate) {
+        x = xCoordinate;
+        y = yCoordinate;
+    }
+
+
+
+}
