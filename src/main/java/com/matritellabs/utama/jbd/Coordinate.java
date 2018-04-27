81b9fc9b7af5185db8db57e1fad5756cc030033a
klibalint <klibalint@mail.eu>
2018-04-27 15:50:18 +0200

    decodeCoordinate() corrected again

--- src/main/java/com/matritellabs/utama/jbd/Coordinate.java
+++ src/main/java/com/matritellabs/utama/jbd/Coordinate.java
@@ -31,33 +31,50 @@ public class Coordinate {
     //Decode coordinate like "A" --> 0
     public static int decodeCoordinate(String inputCoordinate) {
         inputCoordinate = inputCoordinate.toUpperCase();
-        if (inputCoordinate.charAt(0) != -1){
-            switch (inputCoordinate.charAt(0)) {
-                case ('A'):
+            switch (inputCoordinate) {
+                case ("A"):
                     return 0;
-                case ('B'):
+                case ("B"):
                     return 1;
-                case ('C'):
+                case ("C"):
                     return 2;
-                case ('D'):
+                case ("D"):
                     return 3;
-                case ('E'):
+                case ("E"):
                     return 4;
-                case ('F'):
+                case ("F"):
                     return 5;
-                case ('G'):
+                case ("G"):
                     return 6;
-                case ('H'):
+                case ("H"):
                     return 7;
-                case ('I'):
+                case ("I"):
                     return 8;
-                case ('J'):
+                case ("J"):
                     return 9;
+                case ("1"):
+                    return 0;
+                case ("2"):
+                    return 1;
+                case ("3"):
+                    return 2;
+                case ("4"):
+                    return 3;
+                case ("5"):
+                    return 4;
+                case ("6"):
+                    return 5;
+                case ("7"):
+                    return 6;
+                case ("8"):
+                    return 7;
+                case ("9"):
+                    return 8;
+                case ("10"):
+                    return 9;
+                default:
+                    return -1;
             }
-        } else {
-            return -1;
-        }
-        return -1;
     }
 }
 
