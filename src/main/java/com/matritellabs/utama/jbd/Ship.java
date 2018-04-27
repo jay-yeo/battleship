b2b94ecbed877b746e70a9cdf9e72f460975e392
Jason Yeo <jayyeo21@gmail.com>
2018-04-27 14:19:00 +0200

    Fixed error in ship class with int[]

--- src/main/java/com/matritellabs/utama/jbd/Ship.java
+++ src/main/java/com/matritellabs/utama/jbd/Ship.java
@@ -9,7 +9,7 @@ public class Ship {
     public int shipSize;
     public boolean shipSunk;
     public int shipMaxHits;
-    public List<Coordinate> shipCoordinates;
+    public List<int[]> shipCoordinates;
 
     public Ship(String shipPiece, int shipCapacity) {
         shipType = shipPiece;
