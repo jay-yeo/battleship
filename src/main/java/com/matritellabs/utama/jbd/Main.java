b52f32e8831e0abdd01d9712a0465073574161be
Jason Yeo <jayyeo21@gmail.com>
2018-05-14 21:37:45 +0200

    Created Screen class.

--- src/main/java/com/matritellabs/utama/jbd/Main.java
+++ src/main/java/com/matritellabs/utama/jbd/Main.java
@@ -6,29 +6,23 @@ package com.matritellabs.utama.jbd;
 public class Main {
     public static void main(String[] args) {
 
-        // Start new game
-        Game newGame = new Game();
+//        // Start new game
+//        Game newGame = new Game();
+//
+//        // Game menu
+//        newGame.gameMenu();
+//
+//        // Setup players
+//        newGame.setGamePlayers();
+//
+//        // Place ships
+//        newGame.setGameShips();
+//
+//        // Gameplay
+//        newGame.gamePlay();
 
-        // Game menu
-        newGame.gameMenu();
-
-        // Setup players
-        newGame.setGamePlayers();
-
-        // Special Laci EasterEgg
-        if (newGame.getPlayerOne().getPlayerName().toUpperCase().equals("LACI") || newGame.getPlayerTwo().getPlayerName().toUpperCase().equals("LACI") ) {
-
-            System.out.println("Laci Wins! \nGAME OVER");
-
-        } else {
-
-            // Place ships
-            newGame.setGameShips();
-
-            // Gameplay
-            newGame.gamePlay();
-
-        }
+        Screen main = new Screen();
+        main.setVisible(true);
 
 
 
--- src/main/java/com/matritellabs/utama/jbd/Screen.java
+++ src/main/java/com/matritellabs/utama/jbd/Screen.java
@@ -1,4 +1,31 @@
 package com.matritellabs.utama.jbd;
 
-public class Screen {
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen extends JFrame {
+
+    // Class constructor
+    public Screen() throws HeadlessException {
+        initializeGUI();
+    }
+
+    // Initialize GUI
+    public void initializeGUI() {
+
+        // New content frame to hold label and text input
+        JPanel contentFrame = new JPanel();
+        contentFrame.setLayout(new BorderLayout());
+        this.getContentPane().add(contentFrame);
+
+        // Frame attributes
+
+        // Set frame window size
+        this.setSize(1024, 768);
+        // Close button
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+
+
 }
